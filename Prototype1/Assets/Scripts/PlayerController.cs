543d42047fc4bc8e5bd35c552aa81143b2c748cd
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-09-04 20:23:28 -0500

    Added Prototype1Runthrough

---
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float horizontalInput;
+    public float forwardInput;
+    public float turnSpeed;
+ 
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Move Forward with forward input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
---
+++ Prototype1/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver = false;
+    public static bool won = false;
+    public static int score = 0;
+
+    public Text textbox;
+
+    private void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+        if (score >= 3)
+        {
+            won = true;
+            gameOver = true;
+        }
+        if (gameOver)
+        {
+            if (won)
+            {
+                textbox.text = "You Win! \nPress R to Try Again";
+
+            }
+            else
+            {
+                textbox.text = "You Lose!\nPress R to Try Again";
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}

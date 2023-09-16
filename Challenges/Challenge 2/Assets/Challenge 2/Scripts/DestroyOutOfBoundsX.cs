8a6c1ad60de0c5d97525d92d1a9594e1d72c467a
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-09-15 20:06:31 -0500

    Challenge Progress (needs win  condition, spawn interval, and space key cooldown)

---
+++ Challenges/Challenge 2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,38 @@
+﻿       /*
+       * Piper Abbott-Phillips
+       * DestroyOutOfBoundsX.cs
+       * Challenge 2, Assignment 3
+       * Code sets private float for the left most limit of the play area and the bottom most limit of the play area. If an object's position crosses either the y threshold or the x threshold, the object is deleted.
+       */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -30;
+    private float bottomLimit = -5;
+    private HealthSystemX healthSystemXScript;
+
+   void Start()
+    {
+        healthSystemXScript = GameObject.FindGameObjectWithTag("HealthSystemX").GetComponent<HealthSystemX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+            healthSystemXScript.TakeDamage();
+        }
+
+    }
+}
---
+++ Challenges/Challenge 2/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    private DisplayScoreX displayScoreXScript;
+    
+
+    private void Start()
+    {
+        displayScoreXScript = GameObject.FindGameObjectWithTag("DisplayScoreText").GetComponent<DisplayScoreX>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        displayScoreXScript.score++;
+    }
+}

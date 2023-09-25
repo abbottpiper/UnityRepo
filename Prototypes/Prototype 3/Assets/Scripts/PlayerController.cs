7220d58d1bd6014dffc0b39130c983e045799fb6
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-09-25 18:34:47 -0500

    Working Prototype after part 1

---
+++ Prototypes/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpForce;
+    public bool isOnGround = true;
+    public ForceMode forceMode;
+    public bool gameOver = false;
+    public float gravityModifier;
+    void Start()
+    {
+        //set a refrence to rigidbody comp
+        rb = GetComponent<Rigidbody>();
+        forceMode = ForceMode.Impulse;
+
+        //gravitiy mod
+        if (Physics.gravity.y > -10)
+        {
+            Physics.gravity *= gravityModifier;
+        }
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            rb.AddForce(Vector3.up * jumpForce, forceMode);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over! ");
+            gameOver = true;
+        }
+    }
+}
---
+++ Prototypes/Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private float repeatWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //save starting position as a vector3
+        startPosition = transform.position;
+
+        // set repeatwidth to half of the width fo the background
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if background is farther to the left than the repeat width, reset to start pos
+        if (transform.position.x < startPosition.x - repeatWidth)
+        {
+            transform.position = startPosition;
+        }
+    }
+}

236d498bfe9363e1f2e203a482bf1c595695da89
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-12-08 04:35:31 -0600

    working and finished assignment 9 (no webgl)

---
+++ Prototypes/Assignment 9 NavMesh/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,51 @@
+﻿/*Piper Abbott-Phillips
+ * EnemyAI.cs
+ * Assignment9 NavMesh
+ * This script is a copy of the PlayerController script, but instead of targeting the mouse imput of the player, it targets the player character's location
+ * The script also sets a chase distance float, in which the enemy ai will not run towards the player, as long as they're outside of the range
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+public class EnemyAI : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    public GameObject player;
+    public float chaseDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+        character = GetComponent<ThirdPersonCharacter>();
+        agent.updateRotation = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        chaseDistance = 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    private void MoveEnemy()
+    {
+
+        float distanceFromTarget = Vector3.Distance(transform.position, player.transform.position);
+        if (distanceFromTarget > agent.stoppingDistance && distanceFromTarget < chaseDistance)
+        {
+            agent.SetDestination(player.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
---
+++ Prototypes/Assignment 9 NavMesh/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿/*Piper Abbott-Phillips
+ * MoveToClickPoint.cs
+ * Assignment9 NavMesh
+ * This script is a copy of the MoveToClickPoint script, while also referencing the ThirdPersonCharacter script, passing in the information from the NavMeshAgent. 
+ * It also excludes rotational update due to the character animation
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+        character = GetComponent<ThirdPersonCharacter>();
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }    
+    }
+}

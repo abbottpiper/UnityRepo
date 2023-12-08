d2c5110c3fb9f8fad20b53d71729156d5f3ea3b3
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-12-08 04:36:58 -0600

    Update MoveTo.cs

--- Prototypes/Assignment 9 NavMesh/Assets/Scripts/MoveTo.cs
+++ Prototypes/Assignment 9 NavMesh/Assets/Scripts/MoveTo.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*Piper Abbott-Phillips
+ * MoveTo.cs
+ * Assignment9 NavMesh
+ * This script moves the navmesh agent position to the position of the goal. It is disabled and not used throughout the project file.
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
